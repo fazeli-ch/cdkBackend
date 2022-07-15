8e82dcbdc5205ce22f830c9aa567d054fb4bb2e2
fazeli-ch <fazeelamushtaq076@gmail.com>
2022-07-15 12:42:58 -0400

    Updated

--- lib/cdk_backend-stack.ts
+++ lib/cdk_backend-stack.ts
@@ -1,16 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as ddb from "@aws-cdk/aws-dynamodb";
 
-export class CdkBackendStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkBackendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dynamoDBTable = new ddb.Table(this, 'Table', {
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
+    
   }
 }
